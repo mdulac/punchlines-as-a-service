74f4b6bbf2073a63c16387c7eba909a5f03a0d23
mdulac <mat.dulac@gmail.com>
2017-04-16 21:52:27 +0200

    "artists" route

--- src/main/scala/PunchlinesService.scala
+++ src/main/scala/PunchlinesService.scala
@@ -18,80 +18,93 @@ trait Router {
 
   val punchlines: List[Punchline]
 
-  val routes = pathPrefix("punchlines") {
-    get {
-      pathEnd {
-        complete {
-          HttpEntity(
-            ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
-            toJson(punchlines).toString()
-          )
+  val routes =
+    pathPrefix("artists") {
+      get {
+        pathEnd {
+          complete {
+            HttpEntity(
+              ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
+              toJson(punchlines.groupBy(_.artist).keys.toList).toString()
+            )
+          }
         }
-      } ~
-        path("daily") {
-          val epochSecond = now().atStartOfDay().atZone(of("UTC")).toEpochSecond
-          val p = punchlines((epochSecond % punchlines.length).toInt)
-          complete {
-            HttpEntity(
-              ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
-              toJson(p).toString()
-            )
-          }
-        } ~
-        path("pretty") {
-          val random = punchlines(Random.nextInt(punchlines.length))
-          val artist = random.artist
-          val title = random.title.getOrElse("")
-          complete {
-            HttpEntity(
-              ContentTypes.`text/html(UTF-8)`,
-              s"""<html>
-                 |  <head>
-                 |    <style>
-                 |      html, body, .container {
-                 |        height: 100%;
-                 |      }
-                 |      .container {
-                 |        display: flex;
-                 |        align-items: center;
-                 |        justify-content: center;
-                 |        flex-direction: column;
-                 |      }
-                 |    </style>
-                 |  </head>
-                 |  <body>
-                 |    <div class="container">
-                 |      <h1>${random.punchline}</h1>
-                 |      <h3><u>$title</u>, $artist</h3>
-                 |    </div>
-                 |  </body>
-                 |</html>
-                 |""".stripMargin
-            )
-          }
-        } ~
-        path("random") {
-          val random = punchlines(Random.nextInt(punchlines.length))
-          complete {
-            HttpEntity(
-              ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
-              toJson(random).toString()
-            )
-          }
-        } ~
-        pathPrefix("artist") {
-          path(Segment) { artist =>
-            val p = punchlines.filter(_.artist.toLowerCase == artist.toLowerCase)
+      }
+    } ~
+      pathPrefix("punchlines") {
+        get {
+          pathEnd {
             complete {
               HttpEntity(
                 ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
-                toJson(p).toString()
+                toJson(punchlines).toString()
               )
             }
-          }
+          } ~
+            path("daily") {
+              val epochSecond = now().atStartOfDay().atZone(of("UTC")).toEpochSecond
+              val p = punchlines((epochSecond % punchlines.length).toInt)
+              complete {
+                HttpEntity(
+                  ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
+                  toJson(p).toString()
+                )
+              }
+            } ~
+            path("pretty") {
+              val random = punchlines(Random.nextInt(punchlines.length))
+              val artist = random.artist
+              val title = random.title.getOrElse("")
+              complete {
+                HttpEntity(
+                  ContentTypes.`text/html(UTF-8)`,
+                  s"""<html>
+                     |  <head>
+                     |    <style>
+                     |      html, body, .container {
+                     |        height: 100%;
+                     |      }
+                     |      .container {
+                     |        display: flex;
+                     |        align-items: center;
+                     |        justify-content: center;
+                     |        flex-direction: column;
+                     |      }
+                     |    </style>
+                     |  </head>
+                     |  <body>
+                     |    <div class="container">
+                     |      <h1>${random.punchline}</h1>
+                     |      <h3><u>$title</u>, $artist</h3>
+                     |    </div>
+                     |  </body>
+                     |</html>
+                     |""".stripMargin
+                )
+              }
+            } ~
+            path("random") {
+              val random = punchlines(Random.nextInt(punchlines.length))
+              complete {
+                HttpEntity(
+                  ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
+                  toJson(random).toString()
+                )
+              }
+            } ~
+            pathPrefix("artist") {
+              path(Segment) { artist =>
+                val p = punchlines.filter(_.artist.toLowerCase == artist.toLowerCase)
+                complete {
+                  HttpEntity(
+                    ContentType(`application/json`.withParams(Map("charset" -> "utf-8"))),
+                    toJson(p).toString()
+                  )
+                }
+              }
+            }
         }
-    }
-  }
+      }
 
 }
 
